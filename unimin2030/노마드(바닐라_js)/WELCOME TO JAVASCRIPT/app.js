0ff38184c9e4af26a35bc2ee8930d20ab92f7f9e
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-03 00:32:05 +0900

    계절학기 시작

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -1,10 +1,24 @@
-const player = {
-    name:"Jung",
-
-    sayhello: function (aname) {
-        console.log(aname);
+const calculator = {
+    add: function(a, b){
+        console.log(a+b);
+    },
+    sub: function(a,b){
+        console.log(a-b);
+    },
+    div: function(a,b){
+        console.log(a/b);
+    },
+    multi: function(a,b){
+        console.log(a*b);
+    },
+    pow: function(a,b){
+        
+        console.log(a**b);
     }
 };
 
-player.sayhello("HHH");
-console.log(player.name);
+calculator.add(20,10);
+calculator.sub(20,10);
+calculator.div(20,10);
+calculator.multi(20,10);
+calculator.pow(20,10);
