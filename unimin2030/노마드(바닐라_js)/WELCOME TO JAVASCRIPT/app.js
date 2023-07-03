99039b92c962445235ce41954a29e4792187b92c
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-03 16:11:04 +0900

    7/3

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -1,24 +1,3 @@
-const calculator = {
-    add: function(a, b){
-        console.log(a+b);
-    },
-    sub: function(a,b){
-        console.log(a-b);
-    },
-    div: function(a,b){
-        console.log(a/b);
-    },
-    multi: function(a,b){
-        console.log(a*b);
-    },
-    pow: function(a,b){
-        
-        console.log(a**b);
-    }
-};
+const age = prompt("How old are you?");
 
-calculator.add(20,10);
-calculator.sub(20,10);
-calculator.div(20,10);
-calculator.multi(20,10);
-calculator.pow(20,10);
+console.log(age, parseInt(age));
