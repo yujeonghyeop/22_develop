76a2620bc1f4452857051f6155d8c7c76cb8e0bc
jemoog <ojm2561@gmail.com>
2023-07-11 04:43:21 +0900

    파일 수정

--- jemoog_o/test.js
+++ jemoog_o/test.js
@@ -1,47 +1,18 @@
-/*프롬포트 창을 곁들인 계산기*/
-const calculator = {
-    add: function plus(a, b){
-        console.log(a + b);
-    },
-    minus: function minus(a, b){
-        console.log(a - b);
-    },
-    multiply: function multiply(a, b){
-        console.log(a * b);
-    },
-    divide: function divide(a, b){
-        console.log(a / b);
-    },
-    power: function power(a, b){
-        console.log(a ** b);
-    }
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    console.log('title was clicked');
 }
 
-let a = parseInt(prompt("input a"));
-if(isNaN(a)){
-    console.log("error");
-    a = parseInt(prompt("input a"));
+function handleMouseEnter() {
+    console.log('mouse entered');
 }
-let b = parseInt(prompt("input b"));
-if(isNaN(b)){
-    console.log("error");
-    b = parseInt(prompt("input a"));
-}
-let sign = prompt("input sign");
 
-
-if(!isNaN(a) && !isNaN(b)){
-    if(sign == '+'){
-        calculator.add(a, b);
-    }else if(sign === '-'){
-        calculator.minus(a, b);
-    }else if(sign === '*'){
-        calculator.multiply(a, b);
-    }else if(sign === '/'){
-        calculator.divide(a, b);
-    }else if(sign === '^'){
-        calculator.power(a, b);
-    }else{
-        confirm("뭘 친거노?");
-    }
+function handleMouseLeave() {
+    console.log('mouse left');
 }
+
+title.addEventListener("mouseleave", handleMouseLeave);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("click", handleTitleClick);
