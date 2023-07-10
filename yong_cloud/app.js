6f3b1dd5775baa071b34fb8e0b4ac2a7a02f8915
Woo Yong-Un <yongun@uyong-un-ui-MacBookPro.local>
2023-07-10 16:46:46 +0900

    파일수정

--- yong_cloud/app.js
+++ yong_cloud/app.js
@@ -1,3 +1,9 @@
-const age = prompt("How old are you?");
+const age = parseInt(prompt("How old are you?"));
 
-console.log(typeof age);
+if (isNaN(age)) {
+    console.log("Please write a number");
+} else if (age < 18) {
+    console.log("You are too young,");
+} else {
+    console.log ("You can drink");
+}
