3f8f6ec7ee1b438e29d28389630e1df0fa85f270
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-15 18:29:16 +0900

    if-else

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -1,39 +1,17 @@
-const h1 = document.querySelector(".hello:first-child h1");
+﻿const h1 = document.querySelector(".hello:first-child h1");
 
 function clickHandleEvent() {
-    h1.style.color = "blue";
-}
 
-function mouseEnterHandleEvent() {
-    h1.innerText = "Mouse Entered";
-}
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "red";
+    } else
+        newColor = "blue";
+    h1.style.color = newColor;
 
-function mouseLeaveHandleEvent() {
-    h1.innerText = "Mouse Leaved";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "red";
-}
-
-function handleWindowCopy() {
-    alert("copied");
-}
-
-function handleWindowOffline() {
-    alert("SOS NO wifi");
-}
-
-function handleWindowOnline() {
-    alert("WIFI Connected");
 }
 
 h1.addEventListener("click", clickHandleEvent);
-h1.addEventListener("mouseenter", mouseEnterHandleEvent);
-h1.addEventListener("mouseleave", mouseLeaveHandleEvent);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOffline);
 
