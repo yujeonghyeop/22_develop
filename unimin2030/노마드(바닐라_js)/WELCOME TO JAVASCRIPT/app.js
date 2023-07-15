c9bab3706fa9de57eaa8655213c22f1e59684503
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-15 18:58:47 +0900

    css활용

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -1,15 +1,11 @@
 ﻿const h1 = document.querySelector(".hello:first-child h1");
 
 function clickHandleEvent() {
-
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "red";
-    } else
-        newColor = "blue";
-    h1.style.color = newColor;
-
+    if (h1.className === "active") {
+        h1.className = "";
+    } else {
+        h1.className = "active";
+    }
 }
 
 h1.addEventListener("click", clickHandleEvent);
