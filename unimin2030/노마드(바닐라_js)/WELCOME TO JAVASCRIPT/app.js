6edb4246260ee3047416836c3e064c9b821bbe9e
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-16 17:57:40 +0900

    toggle

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -1,12 +1,7 @@
 ﻿const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked"
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
