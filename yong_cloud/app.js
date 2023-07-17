b5912e4abb76e5e37eb88f625e05a4987b2d4328
Woo Yong-Un <yongun@uyong-un-ui-MacBookPro.local>
2023-07-17 15:45:02 +0900

    chater 2

--- yong_cloud/app.js
+++ yong_cloud/app.js
@@ -1,9 +1,20 @@
 const age = parseInt(prompt("How old are you?"));
 
-if (isNaN(age)) {
-    console.log("Please write a number");
+
+
+if (isNaN(age) || age < 0) {
+    console.log("Please write a real number");
 } else if (age < 18) {
     console.log("You are too young,");
-} else {
-    console.log ("You can drink");
+} else if (age >= 18 && age <= 50 ){
+    console.log("You can drink");
+} 
+else if (age > 50 && age <= 80){
+    console.log("You should exercise");
+}
+else if(age > 80){
+    console.log("You can do whatever you want.");
+}
+else {
+    console.log ("You can't drink");
 }
