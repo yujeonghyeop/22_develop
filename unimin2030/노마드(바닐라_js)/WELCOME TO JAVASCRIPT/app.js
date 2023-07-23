0507c109c9e9dd1147398ed0c2c80f047046dc46
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-07-24 04:40:11 +0900

    login-form 끝

--- unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
+++ unimin2030/노마드(바닐라_js)/WELCOME TO JAVASCRIPT/app.js
@@ -4,10 +4,38 @@
 //아래와 같음
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
 
 function onloginSubmit(event) {
-  event.preventDefault();
-  console.log(event);
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText = "Hello " + username;
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onloginSubmit);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onloginSubmit);
+
+} else {
+    paintGreeting(savedUsername);
+
+}
