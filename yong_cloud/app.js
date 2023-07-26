c3130519891a06e56878516e73bc5a815ffce037
Woo Yong-Un <yongun@uyong-un-ui-MacBookPro.local>
2023-07-26 20:13:28 +0900

    3차시

--- yong_cloud/app.js
+++ yong_cloud/app.js
@@ -1,3 +1,39 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".hello h1");
 
-title.innerText = "Got you!";
+
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier")
+}
+
+function hadnleWindowOffline(){
+    alert("SoS no WIFI");
+}
+
+function hadnleWindowOnline(){
+    alert("All GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", hadnleWindowOffline);
+window.addEventListener("online", hadnleWindowOnline);
