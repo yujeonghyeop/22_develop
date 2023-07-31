0a0038ad7549daa767be441015a744b5cd2bd97e
Woo Yong-Un <yongun@uyong-un-ui-MacBookPro.local>
2023-07-31 14:26:02 +0900

    4강 LOGIN

--- yong_cloud/app.js
+++ yong_cloud/app.js
@@ -3,16 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = "Hello" + username;
+    const username = loginInput.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    greeting.innerText = 'Hello ${username}';
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    greeting.innerText = 'Hello ${savedUsername}';
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 
 
