8c146c2070099878baf1e4c802738a3a3fbd3906
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-08-10 23:31:55 +0900

    test

--- unimin2030/react/src/Button.js
+++ unimin2030/react/src/Button.js
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
+const test = () => {
+    console.log("Testing");
+};
+
 function Button({text}) {
     return <button className = {styles.btn}>{text}</button>;
 }
