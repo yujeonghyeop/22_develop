630bf5cab9441d6b19ed135c7514f75ae8b37a33
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-08-14 20:28:00 +0900

    useEffect는 한 번만 렌더링 되기를 원할때 사용한다.

--- unimin2030/react/src/App.js
+++ unimin2030/react/src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import {useEffect, useState} from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev) => (prev + 1));
+    console.log("call an api");
+
+    useEffect(() => {
+        console.log("Once")
+    }, []);
+
     return (
         <div>
-            <h1 className = {styles.title}>Welcome Back to React JS</h1>
-            <Button text = "Continue" />
+            {/*<h1 className = {styles.title}>Welcome Back to React JS</h1>*/}
+            <h1>{counter}</h1>
+            <button onClick = {onClick}>Click me!</button>
         </div>
     );
 }
