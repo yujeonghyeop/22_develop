e86594f1f1e84a2f66aa6f7a59b3d37163a7b40f
JungJoonWoo <jjw2030@ajou.ac.kr>
2023-08-14 20:43:38 +0900

    useEffect의 dev안에 키워드를 입력하면 그 값이 변할때만 리랜더링 한다.

--- unimin2030/react/src/App.js
+++ unimin2030/react/src/App.js
@@ -2,16 +2,25 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => (prev + 1));
+    const onChange = (event) => setKeyword(event.target.value);
     console.log("call an api");
 
     useEffect(() => {
         console.log("Once")
     }, []);
-
+    useEffect(() => {
+        console.log("For your", keyword);
+    }, [keyword])
     return (
         <div>
             {/*<h1 className = {styles.title}>Welcome Back to React JS</h1>*/}
+            <input
+                value = {keyword}
+                onChange = {onChange}
+                type = "text"
+                placeholder = "Serch here" />
             <h1>{counter}</h1>
             <button onClick = {onClick}>Click me!</button>
         </div>
